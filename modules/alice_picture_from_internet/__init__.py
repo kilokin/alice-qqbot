dc34b754ec7ee671260ce66b02a22b119f87031d
kilokin <kilotyo@163.com>
2023-01-12 22:01:57 +0800

    “alice”

---
+++ modules/alice_picture_from_internet/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from graia.ariadne.app import Ariadne
+from graia.ariadne.event.message import GroupMessage
+from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.message.element import Image
+from graia.ariadne.model import Group
+from graia.saya import Channel
+from graia.saya.builtins.broadcast import ListenerSchema
+
+channel = Channel.current()
+
+
+@channel.use(ListenerSchema(listening_events=[GroupMessage]))
+async def img(app: Ariadne, group: Group, message: MessageChain):
+    if message.display.strip() != "每日一图":
+        return
+    session = Ariadne.service.client_session
+    async with session.get("https://api.yimian.xyz/img?type=moe") as resp: 
+        img_bytes = await resp.read()
+    await app.send_message(group, MessageChain(Image(data_bytes=img_bytes)))
